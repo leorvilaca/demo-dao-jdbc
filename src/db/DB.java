5a55dd9eb025d4c9baa7bbb3fcbee2363a115ea3
leorvilaca <leorvilaca@gmail.com>
2020-11-01 14:13:41 -0300

    YourMessage

---
+++ src/db/DB.java
@@ -0,0 +1,73 @@
+package db;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import com.mysql.jdbc.Statement;
+
+public class DB {
+	
+	private static Connection conn = null;
+	
+	public static Connection getConnection() {
+		if(conn == null) {
+			try {
+			Properties props = LoadProperties();
+			String url = props.getProperty("dburl");
+			conn = DriverManager.getConnection(url, props);
+			}
+			catch(SQLException e) {
+				e.getMessage();
+			}	
+		}
+		return conn;
+	}
+	
+	public static void closeClonection() {
+		if(conn != null) {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				new DbException(e.getMessage());
+			}
+		}
+	}
+
+	
+	private static Properties LoadProperties() {
+		try(FileInputStream fs = new FileInputStream("db-properties")){
+			Properties props = new Properties();
+			props.load(fs);
+			return props;
+		}
+		catch (IOException e) {
+			throw new DbException(e.getMessage());
+		}
+		
+	}
+	
+	public void closeStatement(Statement st) {
+		if(st != null) {
+			try {
+				st.close();
+			} catch (SQLException e) {
+				new DbException(e.getMessage());
+			}			
+		}
+	}
+	
+	public void closeResultSet(Statement rt) {
+		if(rt != null) {
+			try {
+				rt.close();
+			} catch (SQLException e) {
+				new DbException(e.getMessage());
+			}			
+		}
+	}
+}
