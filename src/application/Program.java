665bcb836f83b01100d982bd67dac5fc8ce256c5
leorvilaca <leorvilaca@gmail.com>
2020-11-01 19:15:56 -0300

    Criação classes clintes

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -8,8 +11,8 @@ public class Program {
 		
 		
 		Department d1 = new Department(1, "Books");
-		
-		System.out.println(d1);
+		Seller s1 = new Seller(1, "Leonardo Vilaça", "leorvilaca@gmail.com", new Date(), 3000.00, d1);
+		System.out.println(s1);
 
 	}
 
